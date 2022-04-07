44f460e5755bb560ecccebbaa960a7aa65d24be8
Christina K <christinakconnect@gmail.com>
2022-04-07 11:53:33 -0500

    writing to txt file to make variables simpler

--- wordcount.py
+++ wordcount.py
@@ -22,7 +22,7 @@ for line in my_file:
 my_file.close()
 
 input_stem = input_filename.split('.')[0]
-output_filename = "counts."+input_stem+".tsv"
+output_filename = "counts."+input_stem+".txt"
 sorted_words = sorted(words.items(), key=operator.itemgetter(1))
 my_file = open(output_filename, 'w')
 for word in sorted_words:
